c5d80316cc7dd8093f9b9d9ada244db3e8a33d10
hyeoni90 <gusk743@gmail.com>
2019-08-21 22:50:36 +0900

    no message

---
+++ src/main/java/com/hyeon/springrabbitmqsample/SpringRabbitmqSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hyeon.springrabbitmqsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyeon/springrabbitmqsample/SpringRabbitmqSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hyeon.springrabbitmqsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRabbitmqSampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
